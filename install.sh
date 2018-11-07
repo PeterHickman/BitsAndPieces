427d1fb0c55407d5b0a6113e6149bed03a16bde3
Peter Hickman <peterhickman386@googlemail.com>
2018-11-07 21:49:45 +0000

    cleanup

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 install_file()
 {
-  local PROG=$1
-
-  echo "Installing $PROG to /usr/local/bin"
-  install -m a=rx $PROG /usr/local/bin
+  echo "Installing $1 to /usr/local/bin"
+  install -m a=rx "$1" /usr/local/bin
 }
 
 install_file dump_all
